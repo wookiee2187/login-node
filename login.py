10e47a99afcb6e4ed0fccdc0b24fcfabe43e5759
wookiee2187 <nlingareddy@uchicago.edu>
2019-07-29 11:26:07 -0500

    exits if deployment already present

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 #creates pod with login-node image
 from kubernetes import client, config, utils
-import os
+import os, sys
 import pprint 
 import time
 from pyhelm.chartbuilder import ChartBuilder 
@@ -11,9 +11,13 @@ def main():
     config.load_kube_config()
     v1 = client.CoreV1Api()
     k8s_client = client.ApiClient()
+    k8s_api = client.ExtensionsV1beta1Api(k8s_client)
+    check = k8s_api.read_namespaced_deployment_status(name
+    = "login-node-n", namespace ="default")
+    if check != None:
+        sys.exit(0)
     utils.create_from_yaml(k8s_client, "deployNservice.yaml")
     utils.create_from_yaml(k8s_client, "tconfig.yaml")
-    k8s_api = client.ExtensionsV1beta1Api(k8s_client)        	
     deps = k8s_api.read_namespaced_deployment_status(name 
     = "login-node-n", namespace ="default")
     pp = pprint.PrettyPrinter(indent =4)
