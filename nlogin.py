5a7b2538147259b36ea16d4641a0877572596e9b
wookiee2187 <nlingareddy@uchicago.edu>
2019-07-29 11:03:07 -0500

    node name and port

--- nlogin.py
+++ nlogin.py
@@ -6,6 +6,7 @@ import pprint
 import time
 from pyhelm.chartbuilder import ChartBuilder 
 from pyhelm.tiller import Tiller 
+import subprocess
 
 def main():
     config.load_kube_config()
@@ -14,23 +15,16 @@ def main():
     k8s_api = client.ExtensionsV1beta1Api(k8s_client)
     deps = k8s_api.read_namespaced_deployment(name
     = "login-node-n", namespace ="default")
-    pod_name = os.environ["HOME"]
-    print(pod_name)
-    pod = k8s_api.read_namespaced_deployment_status(name = "login-node-n", namespace = "default")
     pp = pprint.PrettyPrinter(indent =4)
     v1 = client.CoreV1Api()
     k8s_client = client.ApiClient()
     serv = v1.read_namespaced_service(name = "login-node-service", namespace = "default")
     pp.pprint(serv.spec.ports[0].node_port)
-    os.system("POD=$(kubectl get pod -l app=login-node-n -o jsonpath="{.items[0].metadata.name}"")
-    print(POD)
-    pp.pprint(deps)
-    print os.environ
-    config.load_kube_config(
-        os.path.join(os.path.expanduser('~'), '.kube', 'config')) 
-    print os.environ
-    KUBERNETES_HOST = "https://%s:%s" % (os.getenv("KUBERNETES_SERVICE_HOST"), os.getenv("KUBERNETES_SERVICE_PORT"))
-    print(KUBERNETES_HOST)
+    bashcmd ="(kubectl get pod -l app=login-node-n -o jsonpath='{.items[0].metadata.name}')"
+    pod_name2 = subprocess.check_output(bashcmd, shell=True)
+    print(pod_name2)
+    podsy = v1.read_namespaced_pod(pod_name2,"default")
+    pp.pprint(podsy.spec.node_name)
 
 if __name__ == '__main__':
     main()
